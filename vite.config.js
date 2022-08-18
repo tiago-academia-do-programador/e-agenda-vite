4f9f7c52971d202b01babe3a61988fdb4d31cfa4
Tiago <tiagosantiniathayde@gmail.com>
2022-08-18 10:13:46 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
+export default defineConfig({
+  base: "/e-agenda-vite/",
+  root: root,
+  build: {
+    outDir: outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(root, "index.html"),
+        tarefaList: resolve(root, "tarefas/tarefa.list.html"),
+        tarefaCreate: resolve(root, "tarefas/tarefa.create.html"),
+      }
+    }
+  },
+  publicDir: "../public"
+});
